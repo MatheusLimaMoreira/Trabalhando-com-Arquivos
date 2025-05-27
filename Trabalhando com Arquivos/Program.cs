6f7cd6152adbcc9fe57558842fb70269bc97c5bb
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-27 10:17:33 -0300

    File_FileInfo Applications

--- Trabalhando com Arquivos/Program.cs
+++ Trabalhando com Arquivos/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string sourcePath = @"c:\temp\File1.txt";
+            string targetPath = @"c:\temp\File2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                } 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error Ocurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
