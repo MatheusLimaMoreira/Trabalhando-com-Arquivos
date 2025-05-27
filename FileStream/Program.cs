2be09dd813c40f83212a5ea8c9d8a7128f34fd85
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-27 11:06:16 -0300

    FileStream and FileReader

---
+++ FileStream/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace FileStream_Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"C:\temp\file1.txt";
+            //FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                //fs = new FileStream(Path, FileMode.Open);
+                sr = File.OpenText(Path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+          
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred while reading the file: ");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                //if (fs != null) fs.Close();
+            }
+        }
+    }
+}
