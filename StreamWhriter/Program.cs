ab2ed78fe3347eaf5572459a2b9daef0d4dddeab
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-30 09:19:04 -0300

    Trabalhando com StreamWriter

---
+++ StreamWhriter/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace StreamWhriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // Ensure resources are cleaned up if necessary
+            }
+
+        }
+    }
+}
