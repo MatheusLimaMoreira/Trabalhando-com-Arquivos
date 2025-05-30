83959abb08877ad98817ce20e30baee06200a581
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-30 11:14:19 -0300

    Trabalhando com Directory and DirectoryInfo

---
+++ Directory and DirectoryInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Directory_and_DirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\MyFolder";
+
+            try
+            {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS");
+                foreach (string S in folders)
+                {
+                    Console.WriteLine(S);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES");
+                foreach (string S in files)
+                {
+                    Console.WriteLine(S);
+                }
+
+                Directory.CreateDirectory(path + @"\NewFolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
