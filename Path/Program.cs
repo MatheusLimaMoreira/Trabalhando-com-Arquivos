f8d316bec84abe65615d2c5dd495e10901168cfe
Matheus de Lima <matheus.moreira127@gmail.com>
2025-05-30 11:33:16 -0300

    Trabalhando com Path

---
+++ Path/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PathCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = @"C:\temp\MyFolder\file1.txt";
+
+
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+            Console.WriteLine("GetPathRoot: " + Path.GetPathRoot(path));
+            Console.WriteLine("GetRandomFileName: " + Path.GetRandomFileName());
+
+        }
+    }
+}
